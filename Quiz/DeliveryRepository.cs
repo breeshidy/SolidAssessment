d3ab20e8443697a9ea32b9cd042faf195444457c
rahmat <rahmat.boakye-agyemang@microlise.com>
2022-08-16 15:29:21 +0100

    Adding DeliveryRepository Class which violates SRP

---
+++ Quiz/DeliveryRepository.cs
@@ -0,0 +1,65 @@
+﻿using Quiz.Services;
+
+namespace Quiz
+{
+    public class DeliveryRepository
+    {
+        private readonly IDictionary<int, DeliveryDetails> _deliveries;
+
+        public DeliveryRepository()
+        {
+            _deliveries = new Dictionary<int, DeliveryDetails>();
+        }
+
+        public void Add(DeliveryDetails deliveryDetails)
+        {
+            if(deliveryDetails == null)
+            {
+                return;
+            }
+
+            _deliveries.Add(deliveryDetails.Id, deliveryDetails);
+
+            //send email to customer about delivery
+            var emailToSend = new Email() {};
+            //some logic here to send Email
+        }
+
+        public DeliveryDetails Get(int deliveryId)
+        {
+            _deliveries.TryGetValue(deliveryId, out var deliveryDetails);
+
+            if(deliveryDetails != null)
+            {
+                //send email to customer about delivery
+                var emailToSend = new Email() { };
+                //some logic here to send Email
+            }
+
+            return deliveryDetails;
+        }
+
+        public void Update(DeliveryDetails deliveryDetails)
+        {
+            if (deliveryDetails == null)
+            {
+                return;
+            }
+
+            _deliveries[deliveryDetails.Id] = deliveryDetails;
+
+            //send email to customer about delivery
+            var emailToSend = new Email() { };
+            //some logic here to send Email
+        }
+
+        public void Delete(int deliveryId)
+        {
+            _deliveries.Remove(deliveryId);
+
+            //send email to customer about delivery
+            var emailToSend = new Email() { };
+            //some logic here to send Email
+        }
+    }
+}
--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string taskToDo = "DeliveryRepository Class currently violates SRP.";
+
+string extraText = "Using your knowledge of SOLID Principles, can you make this class adhere to SRP ?";
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine(taskToDo);
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("                                                                                         ");
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine(extraText);
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("                                                                                         ");
