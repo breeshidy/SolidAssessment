398d3fbf06429d02140e33df6cc89f1e22b4115c
rahmat <rahmat.boakye-agyemang@microlise.com>
2022-08-16 16:33:54 +0100

    LSP Violation example

---
+++ Quiz/Cat.cs
@@ -0,0 +1,16 @@
+﻿namespace Quiz
+{
+    public class Cat
+    {
+        public virtual string GetName() => nameof(Cat);
+
+        public virtual string GetHabitat() => "Home";
+    }
+
+    public class Lion : Cat
+    {
+        public override string GetName() => nameof(Lion);
+
+        public override string GetHabitat() => "Wild";
+    }
+}
--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Quiz;
+
+Cat cat = new Cat();
+Console.WriteLine($"I am a {cat.GetName()} and I live in the {cat.GetHabitat()}.");
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("                                                                                         ");
+
+Console.WriteLine("Do you think the relationship between Base Class Cat & Sub-Class Lion breaks LSP ?");
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("                                                                                         ");
+
+Console.WriteLine("What happens when an instance of Lion is set to a Cat reference, will the code still behave as it is now ?");
+
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("                                                                                         ");
